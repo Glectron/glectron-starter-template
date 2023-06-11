9b9a316172b947e0582c5511028b3e99c8b8990a
YUCLing <luotianyi@luotianyi.me>
2023-06-11 17:02:03 +0800

    add: window resizing chore: center window when popup

--- html/script.js
+++ html/script.js
@@ -1,9 +1,11 @@
 const wnd = document.querySelector("#window");
 const titlebar = document.querySelector("#titlebar");
 
-let offsetX, offsetY;
+let offsetX, offsetY, beforeRect, topOff, leftOff, rightOff, bottomOff;
+let dragWnd = false, resizeWnd = false;
 
 addEventListener("popup", () => {
+    centerWnd();
     wnd.style.visibility = "visible";
 });
 
@@ -19,17 +21,53 @@ addEventListener("glectronlibloaded", () => {
 addEventListener("capturemouserelease", () => {
     glectron.globalMouseMove(false);
     glectron.mouseCapture(false);
+    dragWnd = resizeWnd = false;
 });
 
+const minHeight = 0;
+const minWidth = 0;
+
 addEventListener("globalmousemove", (e) => {
-    wnd.style.left = e.detail.x - offsetX + "px";
-    wnd.style.top = e.detail.y - offsetY + "px";
+    if (dragWnd) {
+        wnd.style.left = e.detail.x - offsetX + "px";
+        wnd.style.top = e.detail.y - offsetY + "px";
+    } else if (resizeWnd) {
+        const dirs = wnd.style.cursor.split("-")[0];
+        if (dirs.indexOf("n") != -1) {
+            let nTop = e.detail.y - topOff;
+            if (beforeRect.bottom - nTop > minHeight) {
+                wnd.style.top = nTop + "px";
+                wnd.style.height = beforeRect.bottom - nTop + "px";
+            }
+        }
+        if (dirs.indexOf("w") != -1) {
+            let nLeft = e.detail.x - leftOff;
+            if (beforeRect.right - nLeft > minWidth) {
+                wnd.style.left = nLeft + "px";
+                wnd.style.width = beforeRect.right - nLeft + "px";
+            }
+        }
+        if (dirs.indexOf("s") != -1) {
+            let nHeight = e.detail.y + bottomOff - beforeRect.top;
+            if (nHeight > minHeight) {
+                wnd.style.height = nHeight + "px";
+            }
+        }
+        if (dirs.indexOf("e") != -1) {
+            let nWidth = e.detail.x + rightOff - beforeRect.left;
+            if (nWidth > minWidth) {
+                wnd.style.width = nWidth + "px";
+            }
+        }
+    }
 });
 
 titlebar.addEventListener("mousedown", (e) => {
-    if (e.target.classList.contains("no-drag")) {
+    if (wnd.style.cursor.indexOf("resize") != -1 || resizeWnd) return;
+    if (e.target.classList.contains("no-drag") || e.target.closest(".no-drag")) {
         return;
     }
+    dragWnd = true;
     let rect = titlebar.getBoundingClientRect();
     let x = e.screenX;
     let y = e.screenY;
@@ -38,6 +76,68 @@ titlebar.addEventListener("mousedown", (e) => {
     glectron.mouseCapture(true);
     glectron.globalMouseMove(true);
 });
+
+const resizeDist = 3;
+
+wnd.addEventListener("mousemove", (e) => {
+    if (dragWnd || resizeWnd) return;
+    let rect = wnd.getBoundingClientRect();
+    let x = e.screenX;
+    let y = e.screenY;
+    let toLeft = x - rect.x;
+    let toTop = y - rect.y;
+    let toRight = rect.right - x;
+    let toBottom = rect.bottom - y;
+    if (toTop <= resizeDist) {
+        if (toLeft <= resizeDist) {
+            wnd.style.cursor = "nw-resize";
+        } else if (toRight <= resizeDist) {
+            wnd.style.cursor = "ne-resize";
+        } else {
+            wnd.style.cursor = "n-resize";
+        }
+    } else if (toLeft <= resizeDist) {
+        if (toBottom <= resizeDist) {
+            wnd.style.cursor = "sw-resize";
+        } else {
+            wnd.style.cursor = "w-resize";
+        }
+    } else if (toBottom <= resizeDist) {
+        if (toRight <= resizeDist) {
+            wnd.style.cursor = "se-resize";
+        } else {
+            wnd.style.cursor = "s-resize";
+        }
+    } else if (toRight <= resizeDist) {
+        wnd.style.cursor = "e-resize";
+    } else {
+        wnd.style.cursor = "";
+    }
+});
+
+wnd.addEventListener("mousedown", (e) => {
+    if (dragWnd) return;
+    if (wnd.style.cursor.indexOf("resize") == -1) return;
+    resizeWnd = true;
+    let rect = wnd.getBoundingClientRect();
+    let x = e.screenX;
+    let y = e.screenY;
+    leftOff = x - rect.x;
+    topOff = y - rect.y;
+    rightOff = rect.right - x;
+    bottomOff = rect.bottom - y;
+    beforeRect = rect;
+    glectron.globalMouseMove(true);
+    glectron.mouseCapture(true);
+});
+
+function centerWnd() {
+    let w = window.innerWidth, h = window.innerHeight;
+    let rect = wnd.getBoundingClientRect();
+    let ww = rect.right - rect.x, wh = rect.bottom - rect.y;
+    wnd.style.left = w / 2 - ww / 2 + "px";
+    wnd.style.top = h / 2 - wh / 2 + "px";
+}
 
 function closeWnd() {
     glectron.unPopup();
